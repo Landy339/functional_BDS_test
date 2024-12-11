265f7c96160e2c55267a28936e8df0445b59941e
Landy339 <75237019+Landy339@users.noreply.github.com>
2024-12-12 00:26:27 +1100

    Update sensitivity_analysis.R

--- sensitivity_analysis.R
+++ sensitivity_analysis.R
@@ -1,3 +1,27 @@
+accuracy_L1norm=matrix(,nrow=1,ncol=5)
+accuracy_L2norm=matrix(,nrow=1,ncol=5)
+accuracy_Maxnorm=matrix(,nrow=1,ncol=5)
+N=c(100,250,500,750,1000)
+for(K in 1:5){
+    random_bds_statistic_L1norm=matrix(,nrow=200,ncol=1)
+    random_bds_statistic_L2norm=matrix(,nrow=200,ncol=1)
+    random_bds_statistic_Maxnorm=matrix(,nrow=200,ncol=1)
+    for(i in 1:200){
+      Sim=fgarch_sim(100,N[K])$garch_mat
+      X=Sim
+      S1=fda_BDS_test_L1norm_quick(X,3,R_L1norm[4])
+      random_bds_statistic_L1norm[i]=S1[[1]]
+      S2=fda_BDS_test_L2norm_quick(X,3,R_L2norm[4])
+      random_bds_statistic_L2norm[i]=S2[[1]]
+      S3=fda_BDS_test_maxnorm_quick(X,3,R_MaxNorm[4])
+      random_bds_statistic_maxnorm[i]=S3[[1]]
+      print(i)
+    }
+    accuracy_L1norm[1,K]=sum(abs(random_bds_statistic_L1norm)>1.96)/200
+    accuracy_L2norm[1,K]=sum(abs(random_bds_statistic_L2norm)>1.96)/200
+    accuracy_maxnorm[1,K]=sum(abs(random_bds_statistic_maxnorm)>1.96)/200
+  }
+
 M=seq(2,10)
 accuracy_L2norm=matrix(,nrow=8,ncol=9)
 for(m in 1:9){
